ea17c963316baa73ec75ac7dea6ab9cf176cd2c1
Jeremy Black <jb@yodacom.com>
2020-03-18 16:11:27 -0600

    app build and hosted on google hosting

---
+++ build/precache-manifest.41b6f23b3dbd57bd6341387da5e03683.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4124312aadeeed7d642b",
+    "url": "/static/css/main.2a1e6200.chunk.css"
+  },
+  {
+    "revision": "4124312aadeeed7d642b",
+    "url": "/static/js/main.c2dbb6c7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "983018187259b29efa9f",
+    "url": "/static/js/2.ef34f38e.chunk.js"
+  },
+  {
+    "revision": "925466df2c7fbaf623b3482ef05f4d23",
+    "url": "/index.html"
+  }
+];
